68ca6e422c31cca2b36ee54ddb94e732ee7a5596
Wei-whey-way <147792887+Wei-whey-way@users.noreply.github.com>
2024-01-08 18:59:37 +0800

    Update unShame.js

--- src/commands/moderation/unShame.js
+++ src/commands/moderation/unShame.js
@@ -1,23 +1,58 @@
-const { ApplicationCommandOptionType, PermissionFlagsBits, } = require('discord.js');
+const { Client, InteractionCollector, ApplicationCommandOptionType, PermissionFlagsBits, } = require('discord.js');
   
 module.exports = {
+  /**
+   * @param {Client} client
+   * @param {Interaction} interaction
+   */
+
+  callback: async (client, interaction) =>{
+    const roleName = ['Shame', 'Glorious Snow'];
+    await interaction.deferReply();
+    // const roleToRemove = interaction.guild.roles.cache.find((role) => role.name === roleName);
+    const rolesToRemove = roleName.map((roleName) =>
+      interaction.guild.roles.cache.find((role) => role.name === roleName)
+    );
+
+    for (const role of rolesToRemove) {
+      if (!role) {
+          await interaction.editReply(`Role "${roleName}" not found.`);
+          return;
+      }
+    }
+
+    console.log(`Roles to remove: ${rolesToRemove.map((role) => `${role.name} (${role.id})`).join(', ')}`);
+
+    try {
+      await interaction.guild.members.fetch();
+
+      for (const roleToRemove of rolesToRemove){
+        const membersWithRole = roleToRemove.members.map((member) => member.user.tag);
+        console.log(`Members with role: ${membersWithRole.join(', ')}`);
+
+        for (const member of roleToRemove.members.values()) {
+          await member.roles.remove(roleToRemove);
+          console.log(`Removed role "${roleName}" from user ${member.user.tag}`);
+        }
+      }
+
+      await interaction.editReply(`Removed role "${roleName}" from all users.`);
+    } catch (error) {
+      console.error('Error removing role:', error);
+      await interaction.editReply('An error occurred while removing the role.');
+    }
+  },
+
   // deleted: true,
-  name: 'Unshame',
+  name: 'unshame',
   description: 'Removes the Glorious Snow and Shame roles',
   // devOnly: Boolean,
   // testOnly: Boolean,
-  options: [
-    {
-      name: 'target-user',
-      description: 'Remove all shame and glorious roles',
-      required: true,
-      type: ApplicationCommandOptionType.Mentionable,
-    },
-  ],
-  permissionsRequired: [PermissionFlagsBits.Administrator],
-  botPermissions: [PermissionFlagsBits.Administrator],
+  options: [],
+  permissionsRequired: [PermissionFlagsBits.ManageRoles],
+  botPermissions: [PermissionFlagsBits.ManageRoles],
   
-  callback: (client, interaction) => {
-    interaction.reply('Time to unshame!');
-  },
+  // callback: (client, interaction) => {
+  //   interaction.reply('Time to unshame!');
+  // },
 };
