3f9519a1013940e032cf98fe2cd16aebd3177da7
Wei-whey-way <147792887+Wei-whey-way@users.noreply.github.com>
2024-04-28 17:40:32 +0930

    Added 'pumpkin weakness', updated 'nega int'

--- src/handlers/eventHandler.js
+++ src/handlers/eventHandler.js
@@ -2,6 +2,14 @@ const path = require('path');
 const { AttachmentBuilder, } = require('discord.js');
 const getAllFiles = require('../utils/getAllFiles');
 
+// Function to generate a random number between 1 and 4
+function getRandomNumber() {
+    // Math.random() generates a random decimal between 0 and 1 (exclusive of 1)
+    // We multiply it by 2 to get a number between 0 and 3.999...
+    // Then we add 1 to make sure the result is between 1 and 4
+    return Math.floor(Math.random() * 2) + 1;
+  }
+
 module.exports = (client) => {
     const eventFolders = getAllFiles(path.join(__dirname, '..', 'events'), true);
     // console.log(eventFolders);
@@ -22,11 +30,6 @@ module.exports = (client) => {
             }
         });
     }
-    // client.user.setActivity({
-    //     name: "My adventure!",
-    //     type: ActivityType.Streaming,
-    //     url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley',
-    // });
 
     //Read messages from chat and respond when someone says Hi Yufine
     client.on('messageCreate', (message) => {
@@ -54,7 +57,21 @@ module.exports = (client) => {
         }
 
         if (message.content.toLowerCase().includes('draw')){
-            const img = new AttachmentBuilder('img/NegaDraw.png', 'NegaDraw.png');
+            const num = getRandomNumber()
+            let img;
+            if (num == 1){
+                img = new AttachmentBuilder('img/NegaDraw.png', 'NegaDraw.png');
+                channel.send({ files: [img] });
+            } else if (num == 2){
+                img = new AttachmentBuilder('img/NegaDraw2.png', 'NegaDraw2.png');
+                channel.send({ files: [img] });
+            } else {
+                message.reply('Oops look like Alusha coding skills are poopoo...');
+            }
+        };
+
+        if (message.content.toLowerCase().includes('arbiter vildred') || message.content.toLowerCase().includes('arbi') || message.content.toLowerCase().includes('arby') || message.content.toLocaleLowerCase() === 'pumpkin weakness' ){
+            const img = new AttachmentBuilder('img/pumpkinweakness.png', 'pumpkinweakness.png');
             channel.send({ files: [img] });
         };
     });
